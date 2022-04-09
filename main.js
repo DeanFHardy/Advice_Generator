60b2c482e6f04975f3089b5ef187d4486317c232
DeanFHardy <deanhardy22@gmail.com>
2022-04-09 05:49:37 +0100

    Functionality completed

---
+++ main.js
@@ -0,0 +1,22 @@
+const adviceQuote = document.querySelector('.quote');
+const adviceId = document.getElementById('header');
+const dice = document.querySelector('.circle');
+let sllp, advice;
+
+// Simple fetch API 
+function getAdvice(){
+    fetch('https://api.adviceslip.com/advice')
+    .then(blob => blob.json())
+    .then(response => {
+        console.log(response);
+        slip = response.slip.id;
+        advice = response.slip.advice;
+        adviceId.textContent = `ADVICE #${slip}`;
+        adviceQuote.textContent = `${advice}`;
+        console.log(slip, advice);
+    });
+}
+
+getAdvice(); // Call function
+
+dice.addEventListener('click', getAdvice);
